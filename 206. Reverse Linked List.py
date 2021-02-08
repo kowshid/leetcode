cb45af16ed0bdc69589b36f85b0215ff0d281daa
kowshid <kowshid@gmail.com>
2021-02-08 18:11:07 +0600

    # Definition for singly-linked list.

---
+++ 206. Reverse Linked List.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def reverse(self, node):
+        if node is None:
+            return None
+        if node.next is None:
+            return node
+
+        ret = self.reverse(node.next)
+        node.next.next = node
+        node.next = None
+
+        return ret
+
+    def reverseList(self, head: ListNode) -> ListNode:
+        return self.reverse(head)
