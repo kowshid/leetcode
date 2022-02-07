960e905a136df1c9a4e648d6f0dbd33bac4210a5
kowshid <kowshid@gmail.com>
2022-02-07 18:58:18 +0600

    First one from easy interview set

---
+++ RemoveDuplicatesFromSortedArray_26.java
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+
+class RemoveDuplicatesFromSortedArray_26 {
+    public int removeDuplicates(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        int k = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1]) {
+                continue;
+            }
+            nums[k++] = nums[i];
+        }
+
+        return k;
+    }
+}
