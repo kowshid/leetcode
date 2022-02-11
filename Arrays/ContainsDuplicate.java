c2771c206845f98695d6396c589515dc8f68141e
kowshid <kowshid@gmail.com>
2022-02-11 19:50:59 +0600

    Four done from array

---
+++ Arrays/ContainsDuplicate.java
@@ -0,0 +1,22 @@
+// Problem: 217
+// https://leetcode.com/problems/contains-duplicate/
+
+package Arrays;
+
+import java.util.HashMap;
+
+public class ContainsDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+        if (nums.length <= 1) return false;
+        HashMap<Integer, Boolean> mapToTrackNumbers = new HashMap<>();
+
+        for (int num: nums) {
+            if (mapToTrackNumbers.get(num) != null) {
+                return true;
+            }
+            mapToTrackNumbers.put(num, true);
+        }
+
+        return false;
+    }
+}
