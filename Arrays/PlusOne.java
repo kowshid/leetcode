79e536fd7fa567890815bb23d1b7e9a21098abee
kowshid <kowshid@gmail.com>
2022-02-11 21:13:44 +0600

    Fifth one done from array

---
+++ Arrays/PlusOne.java
@@ -0,0 +1,31 @@
+// Problem: 66
+// https://leetcode.com/problems/plus-one/
+
+package Arrays;
+
+public class PlusOne {
+    public int[] plusOne(int[] digits) {
+        int index = digits.length - 1;
+
+        if (digits[index] != 9) {
+            digits[index]++;
+            return digits;
+        }
+
+        while(index >= 0) {
+            if (digits[index] != 9) {
+                digits[index]++;
+                break;
+            }
+            digits[index] = 0;
+            index--;
+        }
+
+        if(digits[0] == 0 && index == -1) {
+            digits = new int[digits.length + 1];
+            digits[0] = 1;
+        }
+
+        return digits;
+    }
+}
