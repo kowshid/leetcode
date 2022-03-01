f1d8b9e541319235f25d78120bf1e2e3cbafd111
kowshid <kowshid@gmail.com>
2022-03-01 19:03:37 +0600

    Starting Leetcode March 2022 Challenge

---
+++ 2022 March Leetcoding Challenge/CountingBits.java
@@ -0,0 +1,21 @@
+// Problem: 338
+// https://leetcode.com/problems/counting-bits/
+
+class CountingBits {
+    public int[] countBits(int n) {
+        if (n == 0) {
+            return new int[]{0};
+        }
+        int [] bitsArr = new int[n+1];
+        int twosPower = 1;
+
+        for (int i = 1; i <= n; i++) {
+            bitsArr[i] = 1 + bitsArr[i % twosPower];
+            if(bitsArr[i] == 1) {
+                twosPower = i;
+            }
+        }
+
+        return bitsArr;
+    }
+}
