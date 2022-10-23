d6c135058371dfc2d7c8d0b32df2010282859c47
kowshid <kowshid@gmail.com>
2022-10-23 23:52:07 +0600

    Solved 191. Number of 1 Bits

---
+++ src/others/NumberOf1Bits.java
@@ -0,0 +1,12 @@
+// Problem:191
+// https://leetcode.com/problems/number-of-1-bits/
+
+package others;
+
+public class NumberOf1Bits {
+    public int hammingWeight(int n) {
+        if (n == 0) return 0;
+        if ((n & 1) == 1) return 1 + hammingWeight(n >>> 1);
+        else return hammingWeight(n >>> 1);
+    }
+}
