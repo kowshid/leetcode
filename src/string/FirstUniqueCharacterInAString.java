f4a328a09134f0e9462f2e61f376416824e7b688
kowshid <kowshid@gmail.com>
2022-10-25 22:40:20 +0600

    387. First Unique Character in a String

---
+++ src/string/FirstUniqueCharacterInAString.java
@@ -0,0 +1,32 @@
+package string;
+
+import java.util.HashSet;
+
+public class FirstUniqueCharacterInAString {
+    public int firstUniqChar(String s) {
+        if (s.length() == 1) return 0;
+        HashSet<Character> occurredChar = new HashSet<>();
+        for (int i = 0; i < s.length() - 1; i++) {
+            Character ch = s.charAt(i);
+            if (occurredChar.contains(ch)) continue;
+            occurredChar.add(ch);
+
+            boolean notUniq = false;
+            for (int j = i + 1; j < s.length(); j++) {
+                if (ch.equals(s.charAt(j))) {
+                    notUniq = true;
+                    break;
+                }
+            }
+
+            if (!notUniq) {
+                return i;
+            }
+        }
+
+        if (!occurredChar.contains(s.charAt(s.length() - 1)))
+            return s.length() - 1;
+
+        return -1;
+    }
+}
