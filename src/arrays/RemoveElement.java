5972628c75f6b009351a6e579d09f12c236a3373
Ahsanul Kabir <sm.kabir@bkash.com>
2025-01-26 16:33:34 +0600

    Problem#027: Remove Elements by Copying Values Not Equal to Value

--- src/arrays/RemoveElement.java
+++ src/arrays/RemoveElement.java
@@ -4,6 +4,23 @@
 package arrays;
 
 public class RemoveElement {
+
+    public static int removeElementCopyNonValues(int[] nums, int val) {
+        if (nums.length == 0) {
+            return 0;
+        }
+
+        int count = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[count++] = nums[i];
+            }
+        }
+
+        return count;
+    }
+
     public static int removeElement(int[] nums, int val) {
         if (nums.length == 0) {
             return 0;
